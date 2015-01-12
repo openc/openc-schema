aa48e5b921eb682487951195a0993f0ae5e073f5
Countculture <countculture@gmail.com>
2015-01-12 10:25:52 +0000

    Work in progress
    NB currently testing only entities (which fail)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,6 @@ source "https://rubygems.org"
 group :test do
   gem "rspec"
   gem 'json-schema'
-  # gem 'debugger'
+  gem 'debugger'
   gem 'rake'
 end
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -3,8 +3,11 @@ require 'debugger'
 
 describe "open-schemas" do
   it "should validate valid json" do
-    Dir.glob(File.join('spec','sample-data', 'valid', '*json')).each do |path|
+    # debugger
+    # TODO:change back to all json files when entities are valid
+    Dir.glob(File.join('spec','sample-data', 'valid', 'entity*json')).each do |path|
       data = File.open(path) {|f| f.read}
+      # debugger
       errors = JSON::Validator.fully_validate(find_schema(path), data, :errors_as_objects => true)
       # debugger unless errors.empty?
       expect(errors).to be_empty, "#{path} was invalid: #{prettify_errors(errors)}"
