55dd54a68a22fe60655d65ba293900e0e8535f74
Peter Inglesby <peter.inglesby@gmail.com>
2015-02-05 15:31:46 +0000

    Test that validation fails for the right reason.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "https://rubygems.org"
 
 group :test do
   gem "rspec"
-  gem 'openc-json_schema', :git => 'https://github.com/openc/openc-json_schema'
+  gem 'openc-json_schema', '0.0.2'
   # gem 'debugger'
   gem 'rake'
 end
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -11,12 +11,20 @@ describe "open-schemas" do
 
   end
 
-  Dir.glob(File.join('spec','sample-data', 'invalid', '*json')).each do |path|
-    it "should not validate invalid json at #{path}" do
-      data = JSON.parse(File.read(path))
-      schema = get_schema_path(path)
-      error = Openc::JsonSchema.validate(File.basename(path), File.dirname(path), data)
-      expect(error).to be(nil)
+  Dir.glob(File.join('spec', 'sample-data', 'invalid', '*')).each do |dir|
+    data_type = File.basename(dir)
+
+    Dir.glob(File.join(dir, '*')).each do |path|
+      filename = File.basename(path)
+      match = filename.match(/(.*)-(.*).json/)
+      error_type = match[1]
+      property_path = match[2]
+
+      specify "invalid #{data_type} should fail vaildation with #{error_type} at #{property_path}" do
+        data = JSON.parse(File.read(path))
+        error = Openc::JsonSchema.validate("#{data_type}-schema.json", 'schemas', data)
+        expect(error).not_to be(nil)
+      end
     end
   end
 end
