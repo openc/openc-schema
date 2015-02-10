583a2ff9e7a1cdf692844b6dac2890eb93e0cde7
Peter Inglesby <peter.inglesby@gmail.com>
2015-02-10 12:59:16 +0000

    Several fixes to schemas

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source "https://rubygems.org"
 
 group :test do
   gem "rspec"
-  gem 'openc-json_schema', '0.0.2'
+  gem 'openc-json_schema', '0.0.4'
   # gem 'debugger'
   gem 'rake'
+  gem 'json-schema', :git => 'git://github.com/ruby-json-schema/json-schema.git', :ref => 'aded4d798a48545184dae7ae0a3bb41ec2794c88'
 end
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,20 +1,56 @@
 require 'spec_helper'
 
 describe "open-schemas" do
-  Dir.glob(File.join('spec','sample-data', 'valid', '*json')).each do |path|
+  Dir.glob(File.join('spec','sample-data', 'valid', '*.json')).each do |path|
     it "should validate valid json at #{path}" do
       data = JSON.parse(File.read(path))
-      schema = get_schema_path(path)
-      error = Openc::JsonSchema.validate(File.basename(path), File.dirname(path), data)
+
+      filename = File.basename(path)
+      match = filename.match(/(.*)-\d\d.json/)
+      data_type = match[1]
+
+      error = Openc::JsonSchema.validate(File.join('schemas', "#{data_type}-schema.json"), data)
       expect(error).to be(nil)
     end
+  end
 
+  Dir.glob(File.join('spec','sample-data', 'valid', 'includes', '*.json')).each do |path|
+    it "should validate valid json at #{path}" do
+      data = JSON.parse(File.read(path))
+
+      filename = File.basename(path)
+      match = filename.match(/(.*)-\d\d.json/)
+      data_type = match[1]
+
+      error = Openc::JsonSchema.validate(File.join('schemas', 'includes', "#{data_type}.json"), data)
+      expect(error).to be(nil)
+    end
   end
 
   Dir.glob(File.join('spec', 'sample-data', 'invalid', '*')).each do |dir|
     data_type = File.basename(dir)
 
-    Dir.glob(File.join(dir, '*')).each do |path|
+    unless data_type == 'includes'
+      Dir.glob(File.join(dir, '*.json')).each do |path|
+        filename = File.basename(path)
+        match = filename.match(/(.*)-(.*).json/)
+        error_type = match[1]
+        error_path = match[2]
+
+        specify "invalid #{data_type} should fail vaildation with #{error_type} at #{error_path}" do
+          data = JSON.parse(File.read(path))
+          error = Openc::JsonSchema.validate(File.join('schemas', "#{data_type}-schema.json"), data)
+          expect(error[:type].to_s).to eq(error_type)
+          expect(error[:path]).to eq(error_path)
+        end
+      end
+    end
+  end
+
+  Dir.glob(File.join('spec', 'sample-data', 'invalid', 'includes', '*')).each do |dir|
+    data_type = File.basename(dir)
+
+    Dir.glob(File.join(dir, '*.json')).each do |path|
       filename = File.basename(path)
       match = filename.match(/(.*)-(.*).json/)
       error_type = match[1]
@@ -22,19 +58,10 @@ describe "open-schemas" do
 
       specify "invalid #{data_type} should fail vaildation with #{error_type} at #{error_path}" do
         data = JSON.parse(File.read(path))
-        error = Openc::JsonSchema.validate("#{data_type}-schema.json", 'schemas', data)
+        error = Openc::JsonSchema.validate(File.join('schemas', 'includes', "#{data_type}.json"), data)
         expect(error[:type].to_s).to eq(error_type)
         expect(error[:path]).to eq(error_path)
       end
     end
   end
-end
-
-def get_schema_path(path)
-  filename = File.basename(path)
-
-  raise "Invalid filename for sample data at #{path}" unless filename =~ /-\d\d\.json$/
-
-  schema_name = filename.sub(/-\d\d\.json$/, '-schema.json')
-  File.join('schemas', schema_name)
 end
