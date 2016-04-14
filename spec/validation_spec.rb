50688e4b4c1f26d3eadf7645738f9c2238000a93
seb <seb.bacon@gmail.com>
2016-04-14 10:30:22 +0100

    Validate schemas using json_validation
     (and custom formatters)

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 require 'json'
+require 'json_validation'
+require 'openc_json_schema_formats'
+
 def directory
   if ENV['TEST_ALL']
     '**'
@@ -11,37 +14,66 @@ end
 describe "open-schemas" do
   %w(schemas build).each do |directory|
     Dir.glob(File.join(directory, '**', '*.json')).each do |path|
-      it "#{path} should be a valid schema" do
-        data = JSON.parse(File.read(path))
-        error = Openc::JsonSchema.validate(JSON::Validator.validator_for_name(data['$schema']).metaschema, data)
-        expect(error).to be(nil)
+      data = JSON.parse(File.read(path))
+      metaschema = JSON::Validator.validator_for_name(data['$schema']).metaschema
+      context "when using JsonSchema" do
+        it "#{path} should be a valid schema" do
+          error = Openc::JsonSchema.validate(metaschema, data)
+          expect(error).to be(nil)
+        end
+      end
+
+      pending "when using JsonValidator" do
+        # This is pending https://github.com/inglesp/json_validation/issues/1
+        it "#{path} should be a valid schema" do
+          valid = JsonValidation.load_validator(metaschema).validate(data)
+          expect(valid).to be(true)
+        end
       end
     end
   end
 
   Dir.glob(File.join('spec', directory, 'valid', '*.json')).each do |path|
-    it "should validate valid json at #{path}" do
-      data = JSON.parse(File.read(path))
+    data = JSON.parse(File.read(path))
+    filename = File.basename(path)
+    match = filename.match(/(.*?)(_[a-z-]+)?-\d\d.json/)
+    data_type = match[1]
+    schema_path = File.join('schemas', "#{data_type}-schema.json")
 
-      filename = File.basename(path)
-      match = filename.match(/(.*?)(_[a-z-]+)?-\d\d.json/)
-      data_type = match[1]
+    context "when using JsonSchema" do
+      it "should validate valid json at #{path}" do
+        error = Openc::JsonSchema.validate(schema_path, data)
+        expect(error).to be(nil)
+      end
+    end
 
-      error = Openc::JsonSchema.validate(File.join('schemas', "#{data_type}-schema.json"), data)
-      expect(error).to be(nil)
+    context "when using JsonValidation" do
+      it "should validate valid json at #{path}" do
+        passed = JsonValidation.load_validator(schema_path).validate(data)
+        expect(passed).to be(true)
+      end
     end
   end
 
   Dir.glob(File.join('spec', directory, 'valid', 'includes', '*.json')).each do |path|
-    it "should validate valid json at #{path}" do
-      data = JSON.parse(File.read(path))
+    data = JSON.parse(File.read(path))
+    filename = File.basename(path)
+    match = filename.match(/(.*)-\d\d.json/)
+    data_type = match[1]
+    schema_path = File.join('schemas', 'includes', "#{data_type}.json")
 
-      filename = File.basename(path)
-      match = filename.match(/(.*)-\d\d.json/)
-      data_type = match[1]
+    context "when using JsonSchema" do
+      it "should validate valid json at #{path}" do
+        error = Openc::JsonSchema.validate(schema_path, data)
+        expect(error).to be(nil)
+      end
+    end
 
-      error = Openc::JsonSchema.validate(File.join('schemas', 'includes', "#{data_type}.json"), data)
-      expect(error).to be(nil)
+    context "when using JsonValidation" do
+      it "should validate valid json at #{path}" do
+        passed = JsonValidation.load_validator(schema_path).validate(data)
+        expect(passed).to be(true)
+      end
     end
   end
 
